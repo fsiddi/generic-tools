d73b358557fd8fd9f5679bd823affddfce8fcbda
fsiddi <francesco.siddi@gmail.com>
2012-09-11 17:22:58 +0200

    Script for image conversion and resize
    It detects png image and saves them as jpg in a mirrored filestructure,
    resizing them within a 1280px limit. Already existing jpg images are
    copied and resized as well.

--- file-operations-02/convert.py
+++ file-operations-02/convert.py
@@ -2,7 +2,25 @@ import subprocess
 import os
 import shutil
 
-FOLDER = "/shared/mango/weeklies/"
+FOLDER = "/shared/mango/weeklies_single/"
+FOLDER_NEW = "/shared/mango/weeklies_dest/"
 
 for dirname, dirnames, filenames in os.walk(FOLDER):
-    print filenames
+    for filename in filenames:
+        if "/02" in dirname:
+            filename_src = os.path.join(dirname, filename)
+            dirname_dst = dirname.replace(FOLDER, FOLDER_NEW)
+            if filename.endswith(".png"):
+                if not os.path.exists(dirname_dst):
+                    os.makedirs(dirname_dst)
+                filename_jpg = filename.replace(".png", ".jpg")
+                filename_dst = os.path.join(dirname_dst, filename_jpg)
+                print filename_src + " >> " + filename_dst
+            elif filename.endswith(".jpg"):
+                if not os.path.exists(dirname_dst):
+                    os.makedirs(dirname_dst)
+                filename_dst = os.path.join(dirname_dst, filename)
+                print filename_src + " >> " + filename_dst
+            subprocess.call(["convert", filename_src, "-resize", "1280x1280", filename_dst])
+        else:
+            print "skipping " + dirname
